61c7bc885f8ccafb3336405ce3787ff2d1887353
Fabian Carrion <fabiancarrion1@gmail.com>
2024-07-28 21:31:38 -0500

    data

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -6,20 +6,20 @@ const UserProvider = ({ children }) => {
   const [usuario, setUsuario] = useState(null);
 
   const buscarUsuarioPorDNI = async (codigo) => {
-    // try {
-    //   const response = await fetch(`https://sheetdb.io/api/v1/dxhu9qdsm90h6/search?codigo=${codigo}`);
-    //   const data = await response.json();
-    //   if (data.length > 0) {
-    //     const grupo = data[0].grupo;
-    //     const grupoResponse = await fetch(`https://sheetdb.io/api/v1/dxhu9qdsm90h6/search?grupo=${grupo}`);
-    //     const grupoData = await grupoResponse.json();
-    //     setUsuario(grupoData);
-    //   } else {
-    //     setUsuario(null);
-    //   }
-    // } catch (error) {
-    //   console.error('Error fetching user:', error);
-    // }
+    try {
+      const response = await fetch(`https://sheetdb.io/api/v1/dxhu9qdsm90h6/search?codigo=${codigo}`);
+      const data = await response.json();
+      if (data.length > 0) {
+        const grupo = data[0].grupo;
+        const grupoResponse = await fetch(`https://sheetdb.io/api/v1/dxhu9qdsm90h6/search?grupo=${grupo}`);
+        const grupoData = await grupoResponse.json();
+        setUsuario(grupoData);
+      } else {
+        setUsuario(null);
+      }
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
   };
 
   return (
