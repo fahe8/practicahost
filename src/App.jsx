2698887ae75a4a48a8893a9cfe69111f46a09096
Fabian Carrion <fabiancarrion1@gmail.com>
2024-07-28 20:09:19 -0500

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import AnnoucementBody from "./components/AnnoucementBody";
+import FaqCards from "./components/FaqCards";
+import Footer from "./components/Footer";
+import NavBar from "./components/Navbar";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="">
+        <div className="us-official">
+          <div className="container lg:w-full md:w-[790px]">
+            {" "}
+            Official Website of the Department of Homeland Security{" "}
+          </div>
+        </div>
+        {/* LOGOS NAVBAR */}
+        <NavBar/>
+        <AnnoucementBody />
+
+        <section>
+          <div className="home-wrapper">
+            {/* <Routes>
+             <Route path="/home" element={<Home />}></Route>
+              <Route path="/apply" element={<Apply />}></Route>
+              <Route path="/faq" element={<Pqr />}></Route>
+              <Route path="/recent" element={<MostRecent/>}></Route>
+              <Route path="/history" element={<History/>}></Route> 
+            </Routes> */}
+            {location.pathname !== "/faq" && (
+              <div className=" hidden lg:block bg-white text-center pt-6 pb-14">
+                <h3 className="">
+                  Frequently Asked Questions
+                  <br />
+                  <span>related to this page</span>
+                </h3>
+                <div className="bg-[#ede7dc] p-[10px] grid grid-cols-4 container lg:w-full md:w-[790px] my-6">
+                  <FaqCards />
+                </div>
+              </div>
+            )}
+          </div>
+        </section>
+
+        <footer className=" font-bold">
+          <Footer />
+        </footer>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/data.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState } from 'react';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [usuario, setUsuario] = useState(null);
+
+  const buscarUsuarioPorDNI = async (codigo) => {
+    // try {
+    //   const response = await fetch(`https://sheetdb.io/api/v1/dxhu9qdsm90h6/search?codigo=${codigo}`);
+    //   const data = await response.json();
+    //   if (data.length > 0) {
+    //     const grupo = data[0].grupo;
+    //     const grupoResponse = await fetch(`https://sheetdb.io/api/v1/dxhu9qdsm90h6/search?grupo=${grupo}`);
+    //     const grupoData = await grupoResponse.json();
+    //     setUsuario(grupoData);
+    //   } else {
+    //     setUsuario(null);
+    //   }
+    // } catch (error) {
+    //   console.error('Error fetching user:', error);
+    // }
+  };
+
+  return (
+    <UserContext.Provider value={{ usuario, buscarUsuarioPorDNI }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserProvider, UserContext };
